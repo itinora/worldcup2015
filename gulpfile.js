6f13b63a62db19761ac91968d2201d6a31a96c75
Souvik Basu <souvikbasu@gmail.com>
2015-01-17 13:16:10 +0530

    Added gulp file to minify js

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
+var sourceFiles = [
+    './worldcup.js',
+    './matches.svc.js',
+    ];
+
+gulp.task('concat', function() {
+  gulp.src(sourceFiles)
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('minify', function () {
+    gulp.src(sourceFiles)
+    .pipe(concat('all.min.js'))
+    .pipe(uglify({mangle:true}))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch(sourceFiles, ['concat','minify']);
+});
+
+gulp.task('default', ['concat','minify', 'watch']);
